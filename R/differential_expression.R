64e3fddf6de80e411d39a27edd91641d297affed
mogushi <mogushi@gmail.com>
2021-07-18 16:20:37 +0900

    Update differential_expression.R

--- R/differential_expression.R
+++ R/differential_expression.R
@@ -2012,11 +2012,11 @@ WilcoxDETest <- function(
     no = TRUE
   )
   limma.check <- PackageCheck("limma", error = FALSE)
-  if (limma.check[1] && overflow.check) {
+  if (limma.check[1]) {
     p_val <- my.sapply(
       X = 1:nrow(x = data.use),
       FUN = function(x) {
-        return(min(2 * min(limma::rankSumTestWithCorrelation(index = j, statistics = data.use[x, ])), 1))
+        return(min(2 * min(rankSumTestWithCorrelation_mod(index = j, statistics = data.use[x, ])), 1))
       }
     )
   } else {
@@ -2047,4 +2047,50 @@ WilcoxDETest <- function(
     )
   }
   return(data.frame(p_val, row.names = rownames(x = data.use)))
+}
+
+
+# based on rankSumTestWithCorrelation.R in limma_3.48.1.tar.gz
+rankSumTestWithCorrelation_mod <- function(index,statistics,correlation=0,df=Inf)
+#	Rank sum test as for two-sample Wilcoxon-Mann-Whitney test,
+#	but allowing for correlation between members of test set.
+#	Gordon Smyth and Di Wu
+#	Created 2007.  Last modified 24 Feb 2012.
+{
+	n <- length(statistics)
+	r <- rank(statistics)
+	r1 <- r[index]
+	#n1 <- length(r1)
+	#n2 <- n-n1
+	n1 <- length(r1) * 1.0 # cast to double to avoid overflow
+  n2 <- (n-n1) * 1.0     # cast to double to avoid overflow
+	U <- n1*n2 + n1*(n1+1)/2 - sum(r1)
+	mu <- n1*n2/2
+
+	if(correlation==0 || n1==1) {
+		sigma2 <- n1*n2*(n+1)/12
+	} else {
+		sigma2 <- asin(1)*n1*n2 + asin(0.5)*n1*n2*(n2-1) + asin(correlation/2)*n1*(n1-1)*n2*(n2-1) + asin((correlation+1)/2)*n1*(n1-1)*n2
+		sigma2 <- sigma2/2/pi
+	}
+
+	#TIES <- (length(r) != length(unique(r)))
+	#if(TIES) {
+		#NTIES <- table(r)
+	dup <- duplicated(r)
+	TIES <- (sum(dup) > 0)
+	if(TIES) {
+    # ignore elements that appear only once.
+    #NTIES <- table(r)
+		NTIES <- table(r[dup]) + 1
+		adjustment <- sum(NTIES*(NTIES+1)*(NTIES-1)) / (n*(n+1)*(n-1))
+		sigma2 <- sigma2 * (1 - adjustment)
+	}
+	zlowertail <- (U+0.5-mu)/sqrt(sigma2)
+	zuppertail <- (U-0.5-mu)/sqrt(sigma2)
+
+#	Lower and upper tails are reversed on output
+#	because R's ranks are the reverse of Mann-Whitney's ranks
+	pvalues <- c(less=pt(zuppertail,df=df,lower.tail=FALSE), greater=pt(zlowertail,df=df))
+	pvalues	
 }
